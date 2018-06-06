420d495d2a69d4257a5cb7e8286952cc64beecca
Richard <richard9372@gmail.com>
2018-06-06 23:57:05 +0800

    Add eslint

--- src/installation.js
+++ src/installation.js
@@ -1,48 +1,43 @@
-const rp = require('request-promise-native')
-const jwt = require('jsonwebtoken')
-const path = require('path')
-const cert = require('fs').readFileSync(path.join(__dirname, '..', 'keys/key.pem'))
+import { join } from 'path'
+import { readFileSync } from 'fs'
+import rp from 'request-promise-native'
+import { sign } from 'jsonwebtoken'
 
+const cert = readFileSync(join(__dirname, '..', 'keys/key.pem'))
 const appId = process.env.APPID
 
-function getJwt() {
+export function getJwt () {
   const payload = {
     iat: Math.floor(new Date() / 1000),
     exp: Math.floor(new Date() / 1000) + 60,
-    iss: appId
+    iss: appId,
   }
-  return jwt.sign(payload, cert, {
-    algorithm: 'RS256'
-  })
+  return sign(payload, cert, { algorithm: 'RS256' })
 }
 
-function getToken(installationId) {
+export function getToken (installationId) {
   const jwt = getJwt()
   return rp({
-    uri: `https://api.github.com/installations/${installationId}/access_tokens`,
+    uri: `https://api.github.com/installations/${ installationId }/access_tokens`,
     headers: {
-      Authorization: `Bearer ${jwt}`,
+      Authorization: `Bearer ${ jwt }`,
       Accept: 'application/vnd.github.machine-man-preview+json',
-      'User-Agent': 'Submodule Checker'
+      'User-Agent': 'Submodule Checker',
     },
     method: 'POST',
-    json: true
+    json: true,
   })
 }
 
-async function request(token, endpoint, options = {}) {
+export async function sendRequest (token, endpoint, options = {}) {
   return await rp({
-    uri: `https://api.github.com/${endpoint}`,
+    uri: `https://api.github.com/${ endpoint }`,
     ...options,
     headers: {
-      Authorization: `token ${token}`,
+      Authorization: `token ${ token }`,
       Accept: 'application/vnd.github.machine-man-preview+json',
-      'User-Agent': 'Submodule Checker'
+      'User-Agent': 'Submodule Checker',
     },
-    json: true
+    json: true,
   })
-}
-
-module.exports = {
-  getJwt, getToken, request
 }
